44494ff026ee6d9ac742b288c400474db235a5bc
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-08-05 09:31:40 -0300

    feat: add clerk auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,14 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isHomeRoute = createRouteMatcher(["/"]);
 
 export default clerkMiddleware((auth, req) => {
-    const { userId } = auth();
+	const { userId } = auth();
 
-    // if there is user and home route is accessed, redirect to dashboard or any other protected route
-    if (userId && isHomeRoute(req)) {
-        return NextResponse.rewrite(new URL("/", req.url));
-    }
+	// if there is user and home route is accessed, redirect to dashboard or any other protected route
+	if (userId && isHomeRoute(req)) {
+		return NextResponse.rewrite(new URL("/", req.url));
+	}
 });
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
 };
